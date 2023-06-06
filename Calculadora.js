acf1f11834f90e476249ca75f79283680cff9981
Brenda Abigail Quinteros <quinterosbrenda96@gmail.com>
2023-06-06 17:37:00 -0300

    Add files via upload

---
+++ Calculadora.js
@@ -0,0 +1,7 @@
+const prompt = require('prompt-sync')();
+
+let suma
+let numero1 = parseInt(prompt("numero a sumar"));
+let numero2 = parseInt(prompt("numero a sumar"));
+suma = numero1 + numero2;
+console.log(suma);
